b6e54a6b4dd7467419399c8b0d0cf452b58f7eba
yardenas <yardas@student.ethz.ch>
2020-04-10 11:38:49 +0200

    No need for weird 'if' with new agent api

--- simba/infrastructure/agent_factory.py
+++ simba/infrastructure/agent_factory.py
@@ -4,16 +4,15 @@ import simba.agents as agents
 
 def make_agent(config, environment):
     agent = eval('agents.' + standardize_name(config['options']['agent']))
-    if agent == agents.MbrlAgent:
-        mbrl_agent_params = config['agents']['mbrl_agent']
-        base_agent_params = config['agents']['agent']
-        policy_params = config['policies'][mbrl_agent_params['policy']]
-        model_params = config['models'][mbrl_agent_params['model']]
-        return agent(seed=config['options']['seed'],
-                     observation_space_dim=1,
-                     action_space_dim=1,
-                     policy_parameters=policy_params,
-                     model_parameters=model_params,
-                     **base_agent_params,
-                     **mbrl_agent_params)
+    agent_params = config['agents']['mbrl_agent']
+    base_agent_params = config['agents']['agent']
+    policy_params = config['policies'][agent_params['policy']]
+    model_params = config['models'][agent_params['model']]
+    return agent(seed=config['options']['seed'],
+                 observation_space_dim=1,
+                 action_space_dim=1,
+                 policy_parameters=policy_params,
+                 model_parameters=model_params,
+                 **base_agent_params,
+                 **agent_params)
 
