6398fb105360ac97b2a5d7f730cacbfeb2dfd8ba
yardenas <yardas@student.ethz.ch>
2020-05-10 09:25:17 +0200

    no need for mpi dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
         'gym~=0.15.3',
         'joblib==0.14.0',
         'matplotlib==3.1.1',
-        'mpi4py==3.0.2',
         'mujoco_py==2.0.2.7',
         'numpy~=1.17.4',
         'seaborn==0.8.1',
