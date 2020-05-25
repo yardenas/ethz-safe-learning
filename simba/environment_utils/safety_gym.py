fba2451c07840a27bf013b3d3ddf082a5446473b
yardenas <yardas@student.ethz.ch>
2020-05-25 13:56:27 +0200

    Goal achieved removed really helps

--- simba/environment_utils/safety_gym.py
+++ simba/environment_utils/safety_gym.py
@@ -81,7 +81,7 @@ class SafetyGymStateScorer(object):
         # Clip reward
         if self.reward_clip:
             reward = tf.clip_by_value(reward, -self.reward_clip, self.reward_clip)
-        return reward, goal_achieved
+        return reward, tf.zeros_like(reward, dtype=tf.bool)
 
     def cost(self, observations):
         """ Calculate the current costs and return a dict
