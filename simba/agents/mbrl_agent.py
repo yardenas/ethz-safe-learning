d11f7d7e050e9c7b7c68562c38bab4e130df62b6
yardenas <yardas@student.ethz.ch>
2020-05-28 12:06:06 +0200

    On policy data

--- simba/agents/mbrl_agent.py
+++ simba/agents/mbrl_agent.py
@@ -41,7 +41,7 @@ class MbrlAgent(BaseAgent):
 
     def update(self):
         observations, actions, next_observations, _, _, infos = \
-            self.replay_buffer.sample_random_data(self.train_batch_size)
+            self.replay_buffer.sample_recent_data(self.train_batch_size)
         goal_mets = np.array(list(map(lambda info: info.get('goal_met', False), infos)))
         # We masked transitions where the goal was met since they are non-continuous what extremely destabilizes
         # the learning of p(s_t_1 | s_t, a_t)
