e6fc923f50d379cd8a4c409d59e976b56abe4362
yardenas <yardas@student.ethz.ch>
2020-06-02 17:27:33 +0200

    Tuning policy

--- simba/policies/__init__.py
+++ simba/policies/__init__.py
@@ -1,4 +1,3 @@
 from simba.policies.cem_mpc import CemMpc
 from simba.policies.random_mpc import RandomMpc
-from simba.policies.mppi_mpc import MppiMpc
 from simba.policies.random_shooting_mpc import RandomShootingMpc
